2bdf44fec5e073895f24e4b23f978ace946f6d80
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2020-03-09 14:18:32 -0400

    updated zoos router

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -36,8 +36,9 @@ router.get('/:id/animals', async (req, res, next) => {
 		const animals = await db('zoos_animals as za')
 			.join("zoos as z", 'z.id', 'za.zoo_id') // this joins in our zoos id
 			.join('animals as a', 'a.id', 'za.animal_id') // this joins in our animals id
+			.join("species as s", "s.id", "a.species_id") // this joins in the species table, 
 			.where('za.id', req.params.id) // we are going to select everything where the zoo id is equal to the parameter from the url
-			.select('a.*', 'za.from_date', 'za.to_date') // the select method selects everything from the animal's table and then select both dates from the join table. 
+			.select('a.*', "s.name as species_name", 'za.from_date', 'za.to_date') // the select method selects everything from the animal's table and then select both dates from the join table. 
 
 			res.json(animals)
 	} catch(err){
